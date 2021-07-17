0c40a1c1d6e0c18d05f8b957f8debbe9d89295bb
Artur Zaczek <artur200212@interia.pl>
2021-07-17 03:06:07 +0200

    added grid

--- src/component/Calculator/Calculator.tsx
+++ src/component/Calculator/Calculator.tsx
@@ -8,28 +8,36 @@ const Calculator = () => {
     return (
         <main className='main'>
             <img className='img' src={logo} alt="logo" />
-            <section className='calculatror'> 
-            <label > Bill 
-                <input type="number" />
-            </label>
-            <label > Select Tip% 
-                <button>5%</button>
-                <button>10%</button>
-                <button>15%</button>
-                <button>25%</button>
-                <button>50%</button>
-                <input type="number" />
-            </label>
-            <label > Number of people 
-                <input type="number" />
-            </label>
-            <div>
-                <p>Tip Amount</p> <span>tip</span>
-                <p>/person</p>
-                <p>Total</p> <span>total cost</span>
-                <p>/person</p>
-                <button>restart</button>
-            </div>
+            <section className='calculatror'>
+                <div className='form'>
+
+                    <label className='bill' > Bill <br />
+                        <input className='input' type="number" />
+                    </label>
+
+                    <div>
+
+                        <label className='labelSelectTip' > Select Tip%  </label>
+                        <div className='selectTip'>
+                            <button className='btn5%' >5%</button>
+                            <button className='btn10%'>10%</button>
+                            <button className='btn15%'>15%</button>
+                            <button className='btn25%'>25%</button>
+                            <button className='btn50%'>50%</button>
+                            <input className='selectInput' type="number" />
+                        </div>
+                    </div>
+                    <label className='numberOfPeople' > Number of people  <br />
+                        <input className='input' type="number" />
+                    </label>
+                </div>
+                <div className='outputForm'>
+                    <p>Tip Amount</p> <span>tip</span>
+                    <p>/person</p>
+                    <p>Total</p> <span>total cost</span>
+                    <p>/person</p>
+                    <button className='btn' >restart</button>
+                </div>
             </section>
         </main>
     )
