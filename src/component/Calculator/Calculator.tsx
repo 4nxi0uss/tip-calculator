b9bee7114585732a11162776b88571c77013df0b
Artur Zaczek <artur200212@interia.pl>
2021-07-18 14:03:56 +0200

    fixed bugs, and done more bugs

--- src/component/Calculator/Calculator.tsx
+++ src/component/Calculator/Calculator.tsx
@@ -6,9 +6,9 @@ import './Calculator.scss'
 
 const Calculator = () => {
     const [bill, setBill] = useState<number | undefined | string>()
-    const [tip, setTip] = useState<number | undefined>()
-    const [inputTip, setInputTip] = useState<number | undefined| string>()
-    const [numberOfPeople, setNumberOfPeople] = useState<number| undefined| string>()
+    const [tip, setTip] = useState<number | undefined | string>()
+    const [inputTip, setInputTip] = useState<number | undefined | string>()
+    const [numberOfPeople, setNumberOfPeople] = useState<number | undefined | string>()
 
     const handleBill = (e: any) => {
         setBill(e.target.value)
@@ -23,77 +23,82 @@ const Calculator = () => {
     const hanldeInputTip = (e: any) => {
         setInputTip(e.target.value)
     }
-
-    const tipAmount = (bill: number | any, tip: number | any, people: number | any) => (
-        (Number(bill) / 100 * Number(tip)) / Number(people)
-    )
-    const totalCost = (bill: number | any, tip: number | any, people: number | any) => (
-        Number(((bill / 100 * tip) / people)) + Number(bill)
-    )
-
-    const handleReset = () => {
-        setBill('')
-        setNumberOfPeople('')
-        setTip(Number())
-        setInputTip('')
+    const tipAmount = (bill: number | any, tip: number | any, people: number | any) => {
+        if (people === 0 && tip === 0) {
+            return (null)
+        }else{
+        return ((Number(bill) / 100 * Number(tip)) / Number(people)).toFixed(2)
     }
-    // console.log(bill)
-    // console.log(tip)
-    // console.log(inputTip)
-    // console.log(numberOfPeople)
+}
+const totalCost = (bill: number | any, tip: number | any, people: number | any) => {
+    // if (people === 0 && '' && tip === 0 && '') {
+    if (people === 0 && tip === 0) {
+        return (null)
+    } else {
+        return (((Number(bill) / 100 * Number(tip)) / Number(people) * Number(people) + Number(bill)) / Number(people)).toFixed(2)
+    }
+}
+// console.log(numberOfPeople)
+// console.log(Boolean(numberOfPeople))
+// console.log(Number(numberOfPeople))
+// console.log(tip)
 
-    // console.log(`to ${ Boolean( totalCost(bill,  inputTip || tip,  numberOfPeople )) === false ? "$0.00":totalCost(bill,  inputTip || tip,  numberOfPeople )}`)
-    // console.log(`to ${  totalCost(bill,  inputTip || tip,  numberOfPeople )}`)
+const handleReset = () => {
+    setBill('')
+    setNumberOfPeople('')
+    setTip('')
+    setInputTip('')
+}
 
-    return (
-        // <main className='main'>
-        <>
-            <img className='img' src={logo} alt="logo" />
-            <section className='calculatror'>
-                <div className='form'>
+return (
+    // <main className='main'>
+    <>
+        <img className='img' src={logo} alt="logo" />
+        <section className='calculatror'>
+            <div className='form'>
 
-                    <label className='bill' > Bill <br />
-                        <input placeholder='0' onChange={handleBill} value={bill} className='inputBill' type="number" min={0} />
-                    </label>
+                <label className='bill' > Bill <br />
+                    <input placeholder='0' onChange={handleBill} value={bill} className='inputBill' type="number" min={0} />
+                </label>
 
-                    <div>
-                        <label className='labelSelectTip' > Select Tip %  </label>
-                        <div className='selectTip'>
-                            <button onClick={handleTip} className='btn5' value={5}>5%</button>
-                            <button onClick={handleTip} className='btn10' value={10}>10%</button>
-                            <button onClick={handleTip} className='btn15' value={15}>15%</button>
-                            <button onClick={handleTip} className='btn25' value={25}>25%</button>
-                            <button onClick={handleTip} className='btn50' value={50}>50%</button>
-                            <input onChange={hanldeInputTip} className='selectInput' type="number" placeholder='Custom' value={inputTip} />
-                        </div>
+                <div>
+                    <label className='labelSelectTip' > Select Tip %  </label>
+                    <div className='selectTip'>
+                        <button onClick={handleTip} className='btn5' value={5}>5%</button>
+                        <button onClick={handleTip} className='btn10' value={10}>10%</button>
+                        <button onClick={handleTip} className='btn15' value={15}>15%</button>
+                        <button onClick={handleTip} className='btn25' value={25}>25%</button>
+                        <button onClick={handleTip} className='btn50' value={50}>50%</button>
+                        <input onChange={hanldeInputTip} className='selectInput' type="number" placeholder='Custom' value={inputTip} />
                     </div>
-
-                    <label className='numberOfPeople' > Number of people  <br />
-                        <input placeholder='0' onChange={handleNumberOfPeople} value={numberOfPeople} className='inputPeople' type="number" min={0} />
-                    </label>
-
                 </div>
 
-                <div className='outputForm'>
+                <label className='numberOfPeople' > Number of people  <br />
+                    <input placeholder='0' onChange={handleNumberOfPeople} value={numberOfPeople} className={Number(numberOfPeople) === 0 ? 'inputPeopleRed' : 'inputPeople'} type="number" min={0} />
+                </label>
 
-                    <p className='tipAmount'>Tip Amount
-                        <br />
-                        <span className='person1'>/person</span>
-                    </p>
-                    <span className='cost'>{`$${Boolean(tipAmount(bill, inputTip || tip, numberOfPeople)) === false ? "0.00" : tipAmount(bill, inputTip || tip, numberOfPeople)}`}</span>
+            </div>
 
-                    <p className='total'>Total
-                        <br />
-                        <span className='person2'>/person</span>
-                    </p>
-                    <span className='totalCost'>{`$${Boolean(totalCost(bill, inputTip || tip, numberOfPeople)) === false ? "0.00" : totalCost(bill, inputTip || tip, numberOfPeople)}`}</span>
+            <div className='outputForm'>
 
-                    <button className='btn' onClick={handleReset}>reset</button>
-                </div>
-            </section>
-        </>
-        // </main> 
-    )
+                <p className='tipAmount'>Tip Amount
+                    <br />
+                    <span className='person1'>/person</span>
+                </p>
+                <span className='cost'>{`$${isNaN(Number(tipAmount(bill, inputTip || tip, numberOfPeople))) === true ? "0.00" : tipAmount(bill, inputTip || tip, numberOfPeople)}`}</span>
+
+                <p className='total'>Total
+                    <br />
+                    <span className='person2'>/person</span>
+                </p>
+                <span className='totalCost'>{`$${isNaN(Number(totalCost(bill, inputTip || tip, numberOfPeople))) === true ? "0.00" : totalCost(bill, inputTip || tip, numberOfPeople)}`}</span>
+
+                <button className={'btn'} onClick={handleReset}>reset</button>
+            </div>
+        </section>
+    </>
+    // </main> 
+)
 }
 
 export default Calculator;
